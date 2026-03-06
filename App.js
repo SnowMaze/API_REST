a62af74182280205629952c197e6b9210de421c5
j.hubault <j.hubault@green-medical-software.com>
2026-03-06 11:45:54 +0100

    Ajout du POST

--- App.js
+++ App.js
@@ -1,41 +1,80 @@
 import  express from'express';
 import  bodyParser from'body-parser';
 import mysql from 'mysql2';
-
+import * as db  from './Database.js';
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
 
-let pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database :"api_rest"
-}).promise();
 
-const result = await pool.query("SELECT * FROM tools LEFT JOIN categories ON tools.category_id = categories.id;")
-
-
-app.get('/tools', (req, res) => {
-    res.json(result[0]);
+app.get('/api/tools',async (req, res) => {
+    res.json(await db.getTools());
 });
-app.get('/task/:id', (req, res) => {
-    const taskID = parseInt(req.params.id);
-    const task = tasks.find(t => t.id === taskID);
-    if (!task) {
-        return res.status(404).json({ error: 'Task not found' });
+app.get('/api/tools/:id', async (req, res) => {
+    if (!res) {
+        return res.status(404).json({ error: 'Tool not found' });
     }
-    res.json(task);
+    res.json(await db.getTool(req.params.id));
 });
-app.post('/tasks', (req, res) => {
-    const newTask =
-    { id : tasks.length +1,
-         description : req.body.description };
-    tasks.push(newTask);
-    res.status(201).json({message :'Task created successfully', task: newTask});
-    });
+app.get('/api/tools/:department/:status', async (req, res) => {
+    if (!res) {
+        return res.status(404).json({ error: 'Tool not found' });
+    }
+    res.json(await db.getToolStatus(req.params.department, req.params.status));
+});
+app.get('/api/tools/:min_cost/:max_cost/:category_name', async (req, res) => {
+    if (!res) {
+        return res.status(404).json({ error: 'Tool not found' });
+    }
+    res.json(await db.getToolsByCostAndCategory(req.params.min_cost, req.params.max_cost, req.params.category_name));
+});
+
+app.post('/api/tools', async (req, res) => {
+    const { name, description,vendor, website_url, owner_department,category_id, monthly_cost  } = req.body;
+    const urlRegex = /(https?:\/\/(?:www\.)?[a-zA-Z0-9-]+(?:\.[a-zA-Z]{2,})+(?:\/[^\s]*)?|www\.[a-zA-Z0-9-]+(?:\.[a-zA-Z]{2,})+(?:\/[^\s]*)?)/gi;
+    if (name){
+        if (monthly_cost>0){
+            if(owner_department=="Engineering" 
+                || owner_department=="Marketing" 
+                || owner_department=="Sales"
+                || owner_department=="HR"
+                || owner_department=="Finance"
+                || owner_department=="Operations"
+                || owner_department=="Design"){
+                    if(vendor){
+                        if(website_url.match(urlRegex)){
+                            const exist = await db.existsCategory(category_id)
+                            if (exist){
+                                 const newTool = await db.CreateTool(name, description, vendor, website_url, owner_department, category_id, monthly_cost);
+                                 res.status(201).send({ message: 'Tool created successfully', tool: newTool});
+                            }else{
+                                res.status(400).json({ error: 'category ID Doesn\'t exists '});
+                            }
+                        }else{
+                            res.status(400).json({ error: 'Invalid website URL' });
+                        }
+                        
+                    }else{
+                        res.status(400).json({ error: 'Vendor is required' });   
+                    }
+                }else{
+                    res.status(400).json({ error: 'Invalid owner department' });
+                }
+        }else{
+            res.status(400).json({ error: 'Monthly cost must be greater than 0' });
+        }
+    }else{
+        res.status(400).json({ error: 'Name is required' });
+    }
+
+});  
     
+app.use((err,req, res) => {
+    console.log(err.stack);
+    res.status(404).json({ error: 'Not found' });
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
---
+++ Database.js
@@ -0,0 +1,46 @@
+import mysql from 'mysql2';
+
+let pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database :"api_rest"
+}).promise();
+
+async function existsCategory(category_id) {
+    const [resultCategories] = await pool.query("SELECT * FROM categories WHERE id = ?", [category_id]);
+    return resultCategories.length > 0;
+}
+async function getTools() {
+    const [resultTools] = await pool.query("SELECT tools.*,categories.name as category "+
+                                        "FROM tools LEFT JOIN categories ON tools.category_id = categories.id;");
+    return resultTools[0];
+}
+async function getTool(id) {
+    const [resultTools] = await pool.query("SELECT tools.*,categories.name as category, SUM(usage_logs.tool_id) as total_sessions,"+
+                                        " AVG(usage_logs.usage_minutes) as avg_session_minutes "+
+                                        "FROM tools LEFT JOIN categories ON tools.category_id = categories.id "+
+                                        "LEFT JOIN usage_logs ON tools.id = usage_logs.tool_id "+
+                                        "WHERE tools.id = ?", [id]);
+    return resultTools[0];
+}
+async function getToolStatus(department,status) {
+    const [resultTools] = await pool.query("SELECT tools.*,categories.name as category "+
+                                        "FROM tools LEFT JOIN categories ON tools.category_id = categories.id "+   
+                                        "WHERE tools.owner_department = ? AND tools.status = ?", [department,status]);
+    return resultTools[0];
+}
+async function getToolsByCostAndCategory(min_cost,max_cost,category_name) {
+    const [resultTools] = await pool.query("SELECT  tools.*,categories.name as category  "+
+                                        "FROM tools LEFT JOIN categories ON tools.category_id = categories.id"+
+                                        "WHERE tools.Monthly_cost BETWEEN ? AND ? AND categories.name = ?", [min_cost,max_cost,category_name]);
+    return resultTools[0];
+}
+
+async function CreateTool(name, description, vendor, website_url, owner_department, monthly_cost, category_id) {
+    const [resultTools] = await pool.query("INSERT INTO tools (name, description, vendor, website_url, owner_department,category_id,monthly_cost )"+ 
+                                        " VALUES (?, ?, ?, ?, ?, ?, ?)", 
+                                        [name, description, vendor, website_url, owner_department, monthly_cost, category_id]);                                 
+    return getTool(resultTools.insertId);
+}
+export { getTools, getTool, getToolStatus, getToolsByCostAndCategory, CreateTool,existsCategory };
